1642629381cf750caa66976a9b8efeeb1d7a93fa
Manish Kumar Singh <0896memanish@gmail.com>
2025-02-05 10:28:29 +0530

    Completed UseEffect

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,41 @@
+import { useEffect, useRef, useState } from "react";
 import Container from "./UI/Container.tsx";
-import { Timer as TimerProps } from "./store/timers-context.tsx";
+import {
+  Timer as TimerProps,
+  useTimersContext,
+} from "./store/timers-context.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
+  const [remainingTime, setRemainingTime] = useState<number>(duration * 1000);
+  const timeRef = useRef<NodeJS.Timeout | null>(null);
+  const { isRunning } = useTimersContext();
+
+  if (remainingTime <= 0 && timeRef.current) {
+    clearInterval(timeRef.current);
+  }
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (isRunning) {
+      timer = setInterval(function () {
+        setRemainingTime((prev) => (prev <= 0 ? 0 : prev - 50));
+      }, 50);
+      timeRef.current = timer;
+    } else if (timeRef.current) {
+      clearInterval(timeRef.current);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+  const fomattedTime = (remainingTime / 1000).toFixed(2);
   return (
     <Container as="article">
       <h2>{name}</h2>
-      <p>{duration}</p>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{fomattedTime}</p>
     </Container>
   );
 }
